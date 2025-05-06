956bd0b74c30f39e73c2caa718ef9c637f82a519
Sofiia Broiako <yroha0013@gmail.com>
2025-05-06 23:58:08 +0300

    add card component

---
+++ mini-manager/src/components/tasks/kanban/card.tsx
@@ -0,0 +1,103 @@
+import { Text } from "@/components/text";
+import { User } from "@/graphql/schema.types";
+import { DeleteOutlined, EyeOutlined, MoreOutlined } from "@ant-design/icons";
+import { Button, Card, ConfigProvider, Dropdown, MenuProps, theme } from "antd";
+import React from "react";
+import { useMemo } from "react";
+
+type ProjectCardProps = {
+    id: string,
+    title: string,
+    updateAt: string,
+    dueDate?: string,
+    users?: {
+        id: string,
+        name: string,
+        avatarUrl?: User['avatarUrl']
+    }
+}
+
+const ProjectCard = ({id, title, dueDate, users}: ProjectCardProps) => {
+    const {token} = theme.useToken();
+    const edit = () => {}
+    const dropdownItems = useMemo(() => {
+        const dropdownItems: MenuProps['items'] = [
+            {
+                label: 'View card',
+                key: '1',
+                icon: <EyeOutlined/>,
+                onClick: () => {
+                    edit()
+                }
+            },
+            {
+                danger: true,
+                label: 'Delete card',
+                key: '2',
+                icon: <DeleteOutlined/>,
+                onClick: () => {}
+            }
+        ]
+        return dropdownItems
+    }, [])
+    return(
+        <ConfigProvider
+        theme = {{
+            components: {
+                Tag: {
+                    colorText: token.colorTextSecondary,
+                },
+                Card: {
+                    headerBg: 'transparent'
+                }
+            }
+        }}
+        >
+            <Card
+                size="small"
+                title={<Text ellipsis={{tooltip: title}}>{title}</Text>}  
+                onClick={() => edit()}    
+                extra = {
+                    <Dropdown
+                        trigger={['click']}
+                        menu={{
+                            items: dropdownItems
+                        }}
+                        placement="bottom"
+                        arrow={{pointAtCenter: true}}
+                    >
+                        <Button
+                            type="text"
+                            shape="circle"
+                            icon={
+                                <MoreOutlined
+                                    style={{
+                                        transform: 'rotate(90deg)'
+                                    }}
+                                />
+                            }
+                            onPointerDown={(e) => {
+                                e.stopPropagation()
+                            }}
+                            onClick={(e) => {
+                                e.stopPropagation()
+                            }}
+                        />
+                    </Dropdown>
+                }      
+            >
+                <div
+                style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                    gap: '8px'
+                }}
+                >
+
+                </div>
+            </Card>
+        </ConfigProvider>
+    )
+}
+export default ProjectCard
