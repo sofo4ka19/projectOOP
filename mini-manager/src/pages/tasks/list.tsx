86b7134b759f184a64647447284e827a6a80d4f0
naztes0 <naztes0@gmail.com>
2025-05-08 19:49:13 +0300

    Added loading skeleton, rendered columns for the board. Correctes qqlQuery->gqlQuery

--- mini-manager/src/pages/tasks/list.tsx
+++ mini-manager/src/pages/tasks/list.tsx
@@ -9,6 +9,8 @@ import { GetFieldsFromList } from "@refinedev/nestjs-query";
 import { TasksQuery } from "@/graphql/types";
 import ProjectCardMemo from "@/components/tasks/kanban/card";
 import { KanbanAddCardButton } from "@/components/tasks/kanban/add-card-button";
+import KanbanColumnSkeleton from "@/components/skeleton/kanban";
+import ProjectCardSkeleton from "@/components/skeleton/project-card";
 
 const List = () => {
     const { data: stages, isLoading: isLoadingStages } = useList<TaskStage>({
@@ -27,7 +29,7 @@ const List = () => {
             }
         ],
         meta: {
-            qqlQuery: TASK_STAGES_QUERY
+            gqlQuery: TASK_STAGES_QUERY
         }
     })
     const { data: tasks, isLoading: isLoadingTasks } = useList<GetFieldsFromList<TasksQuery>>({
@@ -45,7 +47,7 @@ const List = () => {
             mode: 'off'
         },
         meta: {
-            qqlQuery: TASKS_QUERY
+            gqlQuery: TASKS_QUERY
         }
     })
 
@@ -53,7 +55,7 @@ const List = () => {
         if (!tasks?.data || !stages?.data) {
             return {
                 unassignedStage: [],
-                stages: []
+                columns: [] //stages: []
             }
         }
         const unassignedStage = tasks.data.filter((task) => task.stageId === null)
@@ -72,30 +74,57 @@ const List = () => {
 
     }
 
-    return (
-        <>
-            <KanbanBoardContainer>
-                <KanbanBoard>
-                    <KanbanColumn
-                        id='unassigned'
-                        title={"unassigned"}
-                        count={taskStages.unassignedStage.length || 0}
-                        onAddClick={() => handleAddCard({ stageId: 'unassigned' })}
-                    >
-                        {taskStages.unassignedStage.map((task) => (
-                            <KanbanItem key={task.id} id={task.id} data={{ ...task, stageId: 'unassigned' }}>
-                                <ProjectCardMemo
-                                    {...task}
-                                    dueDate={task.dueDate || undefined}
-                                />
-                            </KanbanItem>
-                        ))}
-                        {!taskStages.unassignedStage.length && (<KanbanAddCardButton onClick={() => handleAddCard({ stageId: 'unassigned' })} />)}
-                    </KanbanColumn>
+    const isLoading = isLoadingStages || isLoadingTasks
+    if (isLoading) return <PageSkeleton />
 
-                </KanbanBoard>
-            </KanbanBoardContainer>
-        </>
+    return (
+        <KanbanBoardContainer>
+            <KanbanBoard>
+                <KanbanColumn
+                    id='unassigned'
+                    title={"unassigned"}
+                    count={taskStages.unassignedStage.length || 0}
+                    onAddClick={() => handleAddCard({ stageId: 'unassigned' })}
+                >
+                    {taskStages.unassignedStage.map((task) => (
+                        <KanbanItem key={task.id} id={task.id} data={{ ...task, stageId: 'unassigned' }}>
+                            <ProjectCardMemo
+                                {...task}
+                                dueDate={task.dueDate || undefined}
+                            />
+                        </KanbanItem>
+                    ))}
+                    {!taskStages.unassignedStage.length && (<KanbanAddCardButton onClick={() => handleAddCard({ stageId: 'unassigned' })} />)}
+                </KanbanColumn>
+                {taskStages.columns?.map((column) => (<KanbanColumn
+                    key={column.id}
+                    id={column.id}
+                    title={column.title}
+                    count={column.tasks.length}
+                    onAddClick={() => handleAddCard({ stageId: column.id })}
+                >
+
+                </KanbanColumn>))}
+            </KanbanBoard>
+        </KanbanBoardContainer>
+
     )
 }
 export default List
+
+const PageSkeleton = () => {
+    const columnCount = 6;
+    const itemCount = 4;
+    return (
+        <KanbanBoardContainer>
+            {Array.from({ length: columnCount }).map((_, index) => (
+                <KanbanColumnSkeleton key={index}>
+                    {Array.from({ length: itemCount }).map((_, itemIndex) => (
+                        <ProjectCardSkeleton key={itemIndex} />
+                    ))}
+                </KanbanColumnSkeleton>
+            ))
+            }
+        </KanbanBoardContainer>
+    )
+}
