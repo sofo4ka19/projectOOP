659434fc5b47e1fd8dad3ed65fbfef798a3755b9
naztes0 <naztes0@gmail.com>
2025-05-08 23:31:42 +0300

    Setting up components folder for EditTaskPage

---
+++ mini-manager/src/components/index.ts
@@ -0,0 +1,22 @@
+import UpcomingEventsSkeleton from "../components/skeleton/upcoming-events"
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+
+
+export {
+    UpcomingEventsSkeleton,
+    AccordionHeaderSkeleton,
+    KanbanColumnSkeleton,
+    ProjectCardSkeleton,
+    LatestActivitiesSkeleton,
+};
+
+export * from "./accordion";
+export * from "./tasks/form/description";
+export * from "./tasks/form/due-date";
+export * from "./tasks/form/stage";
+export * from "./tasks/form/title";
+export * from "./tasks/form/users";
+export * from "./tasks/form/header";
